9e80cc3fe9df2ccad0a62ff4499a8677523efc1b
Jacob Ernst <jernst4601@gmail.com>
2021-01-04 23:52:04 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TestComponent} from './test/test.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    component: TestComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
